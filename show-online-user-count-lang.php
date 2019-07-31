756b598d9b5cd392d6a5d2ef74819e7e7e3b2585
Alex <43592294+ghostgothic@users.noreply.github.com>
2019-07-31 18:05:28 -0500

    Add files via upload

---
+++ show-online-user-count-lang.php
@@ -0,0 +1,47 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	https://www.question2answer.org/
+
+	
+	File: qa-plugin/example-page/qa-example-lang-default.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: US English language phrases for example plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	/* this file is UTF-8 encoded, you can use your language characters without worries */
+	
+	return array(
+		'total_online_users' => ' ^  Online',
+		'online_guest_member'=>'^1  Usuarios  ^2 Invitados',
+		'online_user_list'=>'Usuarios en Linea',
+		'save_button'=>'Save Changes',
+		'Usuarios en Linea'=>'Show Online Users List',
+		'change_ok'=>'Changes successfully saved!',
+		'activity_time_out'=>'how many minutes after the last activity the user is offline?',
+		'show_site_visitor_count'=>'Show Visitor Count',
+		'today_count'=>'Visitas de hoy     : ^  ',
+		'yesterday_count'=>'Visitas de ayer : ^  ',
+		'total_count'=>'Todas las visitas: ^  '
+		
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
